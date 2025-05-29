7b36020d1377ef4897859cb2106d23e547d1d1c6
Divakar Jaiswal <divakarjaiswal707@gmail.com>
2025-05-30 01:15:50 +0530

    Started with excali frontend

---
+++ apps/draw/components/AuthPage.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const AuthPage = () => {
+  return (
+    <div>
+       
+    </div>
+  )
+}
+
+export default AuthPage
+
+
+//eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiRGl2YWthciIsImVtYWlsIjoiZGl2YWthcmphaXN3YWw3Nzc3QGdtYWlsLmNvbSIsImRhdGUiOiIyMDI1LTA1LTI5IDEzOjU0OjE3In0.iQmcps7vEU1W0mF4iAEMHM1Vb2fR5PwVkO6ulJYlgjc
